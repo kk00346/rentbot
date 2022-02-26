86bcfb2e23baea0764d5a94fda83615af93882ad
徐崇瑋 <hsuchungwei@xuchongweideMacBook-Pro.local>
2022-02-26 19:15:19 +0800

    更新pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,19 @@
 module.exports = {
-    apps: [{
-        name: 'rentbot',
-        script: 'index.js',
-    }],
+  apps: [{
+    script: 'index.js',
+    watch: '.'
+  }],
 
-    // Deployment Configuration
-    deploy: {
-        production: {
-            "user": "root",
-            "host": ["139.162.112.61"],
-            "ref": "origin/master",
-            "repo": "git@github.com:kk00346/rentbot.git",
-            "path": "/root/node/rentbot",
-            "post-deploy": "npm install"
-        }
+  deploy: {
+    production: {
+      user: 'root',
+      host: '139.162.112.61',
+      ref: 'origin/master',
+      repo: 'git@github.com:kk00346/rentbot.git',
+      path: '/root/node/rentbot',
+      'pre-deploy-local': '',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
     }
+  }
 };
